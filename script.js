f612752832a69d884cdb14281fabf727ba5dc8f6
yungestAmine <purpleamine@gmail.com>
2023-06-20 20:43:04 +0200

    creazione header e temi principali

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+function start(){
+    let ulTemi = document.getElementById('ulTemi')
+    for(let i=0; i<3; ++i){
+        let li = document.createElement('li')
+        li.classList.add('liThemes')
+        let img = document.createElement('img')
+        img.src = "/img/theme"+i.toString()+'.jpg'
+        img.classList.add('imgThemes')
+        if(i==0)
+            img.style.border = "3px solid #fff"
+        li.appendChild(img)
+        ulTemi.appendChild(li)
+    }
+}
